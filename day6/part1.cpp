7144ea01ebaf8b70898f04d35c7c90502b9f87b0
Lucas Schwebler <lucas.schwebler@gmail.com>
2022-12-07 16:16:31 +0100

    day 6

---
+++ day6/part1.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    string s;
+    cin >> s;
+    for(int i = 3; i < (int)s.size(); i++){
+        set<char> check;
+        for(int j = i-3; j <= i; j++) check.insert(s[j]);
+        if(check.size() == 4){
+            cout << i+1 << "\n";
+            return 0;
+        }
+    }
+}
